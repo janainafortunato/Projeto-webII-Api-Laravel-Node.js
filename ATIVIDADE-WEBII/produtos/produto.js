13ba99f05ca28a33120dffa09aa087fcfd05f104
Alexpguedes <alex.p.guedes1610@gmail.com>
2019-06-07 16:34:09 -0300

    atividade

---
+++ ATIVIDADE-WEBII/produtos/produto.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var axios = require('axios');
+var apirest_products = require('./produtos/apirest_products')
+var app = express();
+
+//accessa a 
+apirest_products();
+
